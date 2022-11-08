280ca02e7add5cc76539d87429b48028e9eddec5
gmluqa <gmluqa@protonmail.com>
2022-11-08 12:08:23 +0100

    created header and added it to App component

--- videoclub/src/App.js
+++ videoclub/src/App.js
@@ -1,10 +1,12 @@
 // import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header/Headers';
 import Home from './containters/Home/Home';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Home />
     </div>
   );
---
+++ videoclub/src/components/Header/Headers.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return <div className="header">Soy un header</div>;
+};
+
+export default Header;
